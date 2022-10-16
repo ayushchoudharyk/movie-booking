f1dfb39b2eaa611306ea9c4756257ffa12e8a299
AYUSH KRISHNA CHOUDHARY <103287965+ayushchoudharyk@users.noreply.github.com>
2022-10-16 17:50:34 +0530

    Checkpoint 9

--- moviebooking/controllers/user.controller.js
+++ moviebooking/controllers/user.controller.js
@@ -1,7 +1,7 @@
-const { User } = require("../models");
-const TokenGenerator = require("uuid-token-generator");
-const { uuid } = require("uuidv4");
-const { atob, btoa } = require("b2a");
+const { User } = require('../models');
+const TokenGenerator = require('uuid-token-generator');
+const { uuid } = require('uuidv4');
+const { atob, btoa } = require('b2a');
 const tokenGenerator = new TokenGenerator();
 
 async function signUp(req, res) {
@@ -24,11 +24,11 @@ async function signUp(req, res) {
     if (err || user === null) {
       newUser.save((err, user) => {
         if (err)
-          return res.status("400").send(err.message || "some error occurred");
-        res.status(200).send("OK");
+          return res.status('400').send(err.message || 'some error occurred');
+        res.status(200).send('OK');
       });
     } else {
-      res.status(400).send("User Already Exists.");
+      res.status(400).send('User Already Exists.');
     }
   });
 }
@@ -36,12 +36,12 @@ async function signUp(req, res) {
 async function login(req, res) {
   //decrypt username and password by seperating the basic word
   try {
-    const encodedAuth = req.headers["authorization"];
-    const userNameAndPassword = atob(encodedAuth.split(" ")[1]);
-    const username = userNameAndPassword.split(":")[0];
-    const password = userNameAndPassword.split(":")[1];
+    const encodedAuth = req.headers['authorization'];
+    const userNameAndPassword = atob(encodedAuth.split(' ')[1]);
+    const username = userNameAndPassword.split(':')[0];
+    const password = userNameAndPassword.split(':')[1];
     const user = await User.findOne({ username: username });
-    if (user === null) throw new Error("user not found");
+    if (user === null) throw new Error('user not found');
     if (user.password === password) {
       user.isLoggedIn = true;
       user.uuid = uuid();
@@ -49,41 +49,40 @@ async function login(req, res) {
       User.findOneAndUpdate({ username: username }, user, {
         useFindAndModify: false,
       })
-        .then(updateUser => {
-          if (updateUser === null) throw new Error("Unable to update user");
+        .then((updateUser) => {
+          if (updateUser === null) throw new Error('Unable to update user');
           res.status(200).send({
             id: user.uuid,
-            "access-token": user.accesstoken,
+            'access-token': user.accesstoken,
           });
         })
-        .catch(err => {
-          res.status(500).send(err.message || "login failed");
+        .catch((err) => {
+          res.status(500).send(err.message || 'login failed');
         });
     } else {
-      res.status(401).send("username and password dont match");
+      res.status(401).send('username and password dont match');
     }
   } catch (err) {
-    res.status(500).send(err.message || "user not found");
+    res.status(500).send(err.message || 'user not found');
   }
 }
 
 async function logout(req, res) {
   const uuid = req.body.uuid;
-  const update = { isLoggedIn: false, accesstoken: "", uuid: "" };
+  const update = { isLoggedIn: false, accesstoken: '', uuid: '' };
   User.findOneAndUpdate({ uuid: uuid }, update, { useFindAndModify: false })
-    .then(data => {
-      if (data === null) throw new error("unable to logout");
-      res.send({ message: "Logged Out successfully." });
+    .then((data) => {
+      if (data === null) throw new error('unable to logout');
+      res.send({ message: 'Logged Out successfully.' });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err.message);
     });
 }
-
 async function getCouponCode(req, res) {
-  const accesstoken = atob(req.header["authorization"].split(" ")[1]);
+  const accesstoken = atob(req.header['authorization'].split(' ')[1]);
   if (!accesstoken) {
-    return res.status(401).send("user not logged in");
+    return res.status(401).send('user not logged in');
   }
   try {
     const users = await User.find({ accesstoken: accesstoken });
@@ -93,34 +92,31 @@ async function getCouponCode(req, res) {
       res.send([]);
     }
   } catch (err) {
-    return res.status(500).send(err.message || "user not found");
+    return res.status(500).send(err.message || 'user not found');
   }
 }
-
 async function bookShow(req, res) {
   try {
-    const accessToken = atob(req.header["authorization"].split(" ")[1]);
-    if (!accessToken) throw new Error("user not logged in");
+    const accessToken = atob(req.header['authorization'].split(' ')[1]);
+    if (!accessToken) throw new Error('user not logged in');
     const uuid = req.body.uuid;
     const bookingRequest = req.body.bookingRequest;
     User.findOneAndUpdate(
       { uuid: uuid },
       { $push: { bookingRequests: bookingRequest } }
     )
-      .then(data => {
-        if (!data) throw new Error("unable to book show");
+      .then((data) => {
+        if (!data) throw new Error('unable to book show');
         res.status(200).send(bookingRequest);
       })
 
-
-      .catch(err => {
-        res.status(500).send(err.message || "unable to book show");
+      .catch((err) => {
+        res.status(500).send(err.message || 'unable to book show');
       });
   } catch (err) {
-    res.status(500).send(err.message || "unable to book show");
+    res.status(500).send(err.message || 'unable to book show');
   }
 }
-
 
 module.exports = {
   signUp,
